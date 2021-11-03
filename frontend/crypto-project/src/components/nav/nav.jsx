4325c9100a753074c13253749959eea0d6cfec01
SalimAbdala27 <salimabdala27@hotmail.com>
2021-11-03 14:52:19 +0000

    nav done

--- frontend/crypto-project/src/components/nav/nav.jsx
+++ frontend/crypto-project/src/components/nav/nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './nav.scss'
+import Logo from '../../assets/images/Screenshot 2021-11-03 at 11.32.23.png'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Nav = () => {
     return (
-        <div className="nav">
-            asd
+        <div className="nav d-flex align-items-center">
+            <div className="col-2">
+            <img src={Logo} alt="" className="nav__img"/>
+            </div>
+            <div className="col-9">
+            <h2 className="nav__header">The Crypto Files</h2>
+            </div>
+            <div className="col-1">
+            <div className="nav__icon d-flex align-items-center"><GiHamburgerMenu/></div>
+            </div>
         </div>
     )
 }

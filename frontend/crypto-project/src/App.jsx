316811c453d2e189c24124e7f2d320e8d0403db2
Wei-Tong Tang <weitongblue@gmail.com>
2021-11-08 16:24:33 +0000

    nesting changes

--- frontend/crypto-project/src/App.jsx
+++ frontend/crypto-project/src/App.jsx
@@ -8,23 +8,22 @@ import Prices from './pages/prices/prices';
 const App = () => {
 	return (
 		<Router>
-		<div className = "px-0 m-0 d-flex flex-column justify-content-start app-container"> 
-			<Nav/>
-			<div className="App">
-			
-			<Switch>
-				<Route path="/prices">
-				<Prices/>
-				</Route>
-				<Route path="/">
-				<Home/>
-				</Route>
+			<div className = "px-0 m-0 d-flex flex-column justify-content-start app-container"> 
+				<Nav/>
+				<div className="App">
+				<Switch>
+					<Route path="/prices">
+						<Prices/>
+					</Route>
+					<Route path="/">
+						<Home/>
+					</Route>
 				</Switch>
-			</div>
-			<div className = "p-0 m-0 d-flex flex-column justify-content-end flex-grow-1 align-items-center"> 
-				<Footer/> 
-			</div>
-		</div> 
+				</div>
+				<div className = "p-0 m-0 d-flex flex-column justify-content-end flex-grow-1 align-items-center"> 
+					<Footer/> 
+				</div>
+			</div> 
 		</Router>
 	);
 }
--- frontend/crypto-project/src/pages/prices/prices.jsx
+++ frontend/crypto-project/src/pages/prices/prices.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Prices = () => {
-  return (
-    <div>
-      Prices
-    </div>
-  )
+    return (
+        <div>
+            Prices
+        </div>
+    )
 }
 
 export default Prices

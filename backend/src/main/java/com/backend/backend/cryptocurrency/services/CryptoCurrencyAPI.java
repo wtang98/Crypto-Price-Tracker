dbfd5135366edca4928d5779a6d8fb9bb5003bac
jeromekithinji <kithinjijerome@gmail.com>
2021-11-19 12:49:21 +0000

    Fixed code

--- backend/src/main/java/com/backend/backend/cryptocurrency/services/CryptoCurrencyAPI.java
+++ backend/src/main/java/com/backend/backend/cryptocurrency/services/CryptoCurrencyAPI.java
@@ -151,10 +151,6 @@ public class CryptoCurrencyAPI {
         List<NameValuePair> parameters = new ArrayList<NameValuePair>();
         parameters.add(new BasicNameValuePair("start","1"));
         parameters.add(new BasicNameValuePair("limit","5"));
-=======
-        parameters.add(new BasicNameValuePair("id","1"));
-
->>>>>>> 091e79db020a7b9afd09af1337e0fd2745bd8491
         try {
             result = makeAPICall(uri, parameters);
         } catch (IOException e) {

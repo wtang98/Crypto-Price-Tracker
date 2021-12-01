506a90ca7019a0caecb90207992c0165cc35ba3c
Wei-Tong Tang <weitongblue@gmail.com>
2021-12-01 11:36:20 +0000

    updated react router dom

--- frontend/crypto-project/src/App.jsx
+++ frontend/crypto-project/src/App.jsx
@@ -2,29 +2,26 @@ import './App.scss';
 import Home from './pages/home/home';
 import Footer from "./components/footer/footer";
 import Nav from "./components/nav/nav"; 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Prices from './pages/prices/prices';
 
 const App = () => {
 	return (
-		<Router>
+		<BrowserRouter>
 			<div className = "px-0 m-0 d-flex flex-column justify-content-start app-container"> 
-				<Nav/>
+				
 				<div className="App">
-				<Switch>
-					<Route path="/prices">
-						<Prices/>
-					</Route>
-					<Route path="/">
-						<Home/>
-					</Route>
-				</Switch>
+				<Routes>
+					<Route path="/prices" element={[<Nav prices={true}/>,<Prices/>]}/>
+					<Route path="/" element={[<Nav home={true} />,<Home/>]}/>
+				</Routes>
 				</div>
 				<div className = "p-0 m-0 d-flex flex-column justify-content-end flex-grow-1 align-items-center"> 
 					<Footer/> 
 				</div>
 			</div> 
-		</Router>
+		</BrowserRouter>
 	);
 }
 
--- frontend/crypto-project/src/components/nav/nav.jsx
+++ frontend/crypto-project/src/components/nav/nav.jsx
@@ -4,18 +4,22 @@ import Logo from '../../assets/images/Screenshot 2021-11-03 at 11.32.23.png'
 import { Squash as Hamburger } from 'hamburger-react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-const Nav = () => {
-    AOS.init();
+const Nav = (props) => {
+    const {prices, exchange, home} = props
+    
     const [openMenu, setOpenMenu] = useState(false);
+    
+    const navigate = useNavigate();
+    AOS.init();
     const menuDropDown = () => {
         return (
             <div className = "dropDown d-flex justify-content-center align-items-center flex-column" data-aos="zoom-out-down-right">
-                <Link to="/prices" style={{ textDecoration: 'none'}}>
-                    <li className = "dropDown__text">Prices</li>
-                </Link>
-                <hr />
+                {!home && <><li onClick={()=>navigate('/')} className = "dropDown__text">Home</li>
+                <hr /></>}
+                {!prices && <><li onClick={()=>navigate('/prices')} className = "dropDown__text">Prices</li>
+                <hr /></>}
                 <li className = "dropDown__text">Learn</li>
                 <hr />
                 <li className = "dropDown__text">Exchanges</li>

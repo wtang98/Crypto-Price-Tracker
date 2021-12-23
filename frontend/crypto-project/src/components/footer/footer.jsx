f346db2953d3e0e37f429709fb00b2e40b16f711
Wei-Tong Tang <weitongblue@gmail.com>
2021-12-23 12:16:02 +0000

    footer changes

--- frontend/crypto-project/src/components/footer/footer.jsx
+++ frontend/crypto-project/src/components/footer/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 	return (
 		<div className ="footer p-0">
 			<p  className = "footer__text d-flex text-center justify-content-center p-0 m-0 ">
-				© Wei-Tong Tang · Salim Abdala · Isher Sagay · Jerome Kithinji · Angaar Uriakhil
+				© Wei-Tong Tang · Salim Abdala · Jerome Kithinji
 			</p>
 		</div>
 	)
